263a908c11258e67fdff3caf31f5328151709c4f
Onur YURDUPAK <onuryurdupak@gmail.com>
2022-03-15 17:33:21 +0300

    Hotfix typed error accessibility and add docs

--- error/typed_error.go
+++ error/typed_error.go
@@ -1,23 +1,41 @@
-package error
+package constants
 
 type TypedErrorCode int32
 
-type typedError struct {
+const (
+	CodeUndefinedErr TypedErrorCode = 0
+	CodeInternalErr  TypedErrorCode = 1
+	CodeAuthErr      TypedErrorCode = 2
+)
+
+// TypedError wraps a go error and an additional TypedErrorCode value for handling errors with higher precision.
+//
+// TypedErrorCode can have a user defined value or any of the constants provided in the package.
+//
+// E.g.: CodeInternalErr, CodeAuthErr etc...
+type TypedError struct {
 	err  error
 	code TypedErrorCode
 }
 
-func NewTypedError(err error, code TypedErrorCode) *typedError {
-	return &typedError{
+// NewTypedError creates a new error instance from given go error and TypedErrorCode value.
+//
+// TypedErrorCode can have a user defined value or any of the constants provided in the package.
+//
+// E.g.: CodeInternalErr, CodeAuthErr etc...
+func NewTypedError(err error, code TypedErrorCode) *TypedError {
+	return &TypedError{
 		err:  err,
 		code: code,
 	}
 }
 
-func (e *typedError) Code() TypedErrorCode {
+// Code returns TypedErrorCode defined during initialization.
+func (e *TypedError) Code() TypedErrorCode {
 	return e.code
 }
 
-func (e *typedError) Error() error {
+// Error returns wrapped go error.
+func (e *TypedError) Error() error {
 	return e.err
 }
